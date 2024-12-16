bf86ff8f8a1c3198a6a53e9c8f8f8b9dc0cf05f3
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-15 23:29:47 -0500

    Update breakout.py

--- breakout.py
+++ breakout.py
@@ -84,7 +84,7 @@ if df_temp.empty:
    st.warning('Error occured try again with a valid ticker !',icon='⚠️')
    st.stop()
 #reset to keep the date as the column
-if debug:st.write(f'before resetting columns ...{df_temp.columns}')
+if debug:st.write(f'before resetting index ...{df_temp.columns}')
 
 if debug:st.write(df_temp)
 if debug:st.write(df_temp.columns)
@@ -99,6 +99,7 @@ else:
 df_temp=df_temp.reset_index(drop=False)
 #changing Date into Datetime
 df_temp['Date']=pd.to_datetime(df_temp['Date']) #in remote
+if debug:st.write(f'after resetting index ...{df_temp.columns}')
 #difference between start and end date
 days_diff=int((end_date-start_date).days)
 
@@ -121,7 +122,7 @@ if debug:st.write(f'volume_threshold: {volume_threshold}')
 #volume threshold percentage
 pct_threshold=st.sidebar.number_input("Enter the percent threshold %",value=2.0,min_value=0.0)
 if debug:st.write(f'pct_threshold: {pct_threshold}')
-
+st.stop()
 
 #get daily change % and 20 day average volume
 df.loc[:,'daily_change%']=df['Close'].copy().pct_change()*100
