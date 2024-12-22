225b8d6417cc39c5b9692f89bf6066550eeb187d
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-21 20:17:38 -0500

    Update breakout.py

--- breakout.py
+++ breakout.py
@@ -208,7 +208,7 @@ def color_val(val):
 df_fin=df_final.style.apply(lambda row:row.apply(color_val),subset=['return(%)'])#.format('{:.2f}')
 
 #st.dataframe(df_final,use_container_width=True)
-st.dataframe(df_fin,use_container_width=True)
+st.markdown(df_fin,unsafe_allow_html=True)
 #include download button
 file_name=f'{user_ticker}.csv'
 csv=df_final.to_csv()
